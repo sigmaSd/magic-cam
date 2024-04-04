95f10c2bda1b72643e9a413a3fa5af44678d0d15
Nbiba Bedis <bedisnbiba@gmail.com>
2024-04-04 10:45:27 +0100

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
 /// <reference lib="dom" />
-const video = document.getElementById("video");
+const video = document.getElementById("video") as HTMLVideoElement;
 const canvas = document.createElement("canvas");
 const ocrResult = document.getElementById("ocr-result");
 const captureBtn = document.getElementById("capture-btn");
 const startBtn = document.getElementById("start-btn");
 const switchCameraBtn = document.getElementById("switch-camera-btn"); // Reference to the switch camera button
 
-let mediaStream; // Variable to store the active media stream
+let mediaStream: MediaStream; // Variable to store the active media stream
+let newFacingMode = "user";
+let stopAnim = false;
+let worker;
+
+// MAIN
+
+// Capture image from video stream
+Tesseract.createWorker("eng").then((res: any) => worker = res);
+// Start the camera when the page loads
+startCamera();
+
+//END
 
 // Access the camera and stream video
 async function startCamera(props = { video: { facingMode: "environment" } }) {
   try {
     mediaStream = await navigator.mediaDevices.getUserMedia(props);
-    video.srcObject = mediaStream;
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    video!.srcObject! = mediaStream;
   } catch (err) {
     console.error("Error accessing the camera: ", err);
   }
 }
 
-let newFacingMode = "user";
 // Function to switch between front and back cameras
 async function switchCamera() {
   // Stop the current stream
@@ -30,11 +42,6 @@ async function switchCamera() {
   // Start the new stream with the updated facing mode
   await startCamera({ video: { facingMode: newFacingMode } });
 }
-
-// Capture image from video stream
-const worker = await Tesseract.createWorker("eng");
-
-let stopAnim = false;
 
 async function recognize() {
   canvas.width = video.videoWidth;
@@ -70,6 +77,3 @@ captureBtn.onclick = async () => {
 switchCameraBtn.onclick = async () => {
   await switchCamera();
 };
-
-// Start the camera when the page loads
-await startCamera();
