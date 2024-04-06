b417581582f2c1360dc3479e0df04b1a31bf6ec2
Nbiba Bedis <bedisnbiba@gmail.com>
2024-04-06 20:58:22 +0100

    prioritize network over cache

--- sw.js
+++ sw.js
@@ -6,16 +6,19 @@ const cacheName = "myapp-v1";
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
+      try {
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch {
+        const response = await caches.match(e.request);
+        console.log(`[Service Worker] Using cached resource: ${e.request.url}`);
+        if (response) {
+          return response;
+        }
       }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
     })(),
   );
 });
